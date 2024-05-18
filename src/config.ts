e051bf24b264ea19f9e79b43f5d8ebe8b94f4cdf
Oleksandr Kuzmenko <eight.alex@gmail.com>
2024-05-18 13:39:57 +0300

    presave fix
    Signed-off-by: BohdanRadchenko <bradchenko@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,5 @@ export const IS_DEV: boolean = MODE === 'development' || DEV;
 export const VERSION: string = process.env.npm_package_version || '0.0.0';
 export const API_URL: string = process.env.API_URL || '';
 export const APP_URL: string = process.env.APP_URL || '';
+
+console.log('process.env', process.env);
